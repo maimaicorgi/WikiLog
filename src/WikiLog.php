16654f9512190b6d8c9427cfca10b07cbe04f9d4
root <root@DESKTOP-5KVMLKM>
2023-03-07 19:35:15 +0900

    first commit

---
+++ src/WikiLog.php
@@ -0,0 +1,42 @@
+<?php
+
+class Wikilog
+{
+    public function start(): void
+    {
+        $this->checkDb();
+
+        while (true)
+        {
+            echo '操作を選択してください。' . PHP_EOL;
+            echo '1 : ログ検索 その１' . PHP_EOL;
+            echo '2 : ログ検索 その２' . PHP_EOL;
+            echo '8 : ログデータを追加する' . PHP_EOL;
+            echo '9 : アプリを終了する' . PHP_EOL;
+
+            $input = fgets(STDIN);
+
+            switch ($input)
+            {
+                case '1':
+                    break;
+                case '2':
+                    break;
+                case '8':
+                    break;
+                case '9':
+                    exit('アプリを終了しました。' . PHP_EOL);
+            }
+        }
+    }
+
+    private function checkDb(): void
+    {
+
+    }
+}
+
+// ----------------------------------------------------------------------
+
+$wikiLog = new WikiLog();
+$wikiLog->start();
