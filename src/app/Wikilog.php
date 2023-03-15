a7e53713da424316e6191181b5f32fe904610f02
root <root@DESKTOP-5KVMLKM>
2023-03-15 11:36:57 +0900

    メニューを改変

--- src/app/Wikilog.php
+++ src/app/Wikilog.php
@@ -18,7 +18,6 @@ class Wikilog
             echo '操作を選択してください。' . PHP_EOL;
             echo '1 : ログ検索 その１' . PHP_EOL;
             echo '2 : ログ検索 その２' . PHP_EOL;
-            echo '8 : ログデータを追加する' . PHP_EOL;
             echo '9 : アプリを終了する' . PHP_EOL;
 
             $input = fgets(STDIN);
@@ -28,8 +27,6 @@ class Wikilog
                 case '1':
                     break;
                 case '2':
-                    break;
-                case '8':
                     break;
                 case '9':
                     exit('アプリを終了します。' . PHP_EOL);
