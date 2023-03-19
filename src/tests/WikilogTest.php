d1cf2c067b1da2f6e4588de1d17588e5c543cf2e
root <root@DESKTOP-5KVMLKM>
2023-03-19 15:04:56 +0900

    testStart_menu2()が完成

--- src/tests/WikilogTest.php
+++ src/tests/WikilogTest.php
@@ -56,14 +56,12 @@ final class WikilogTest extends TestCase
                                             [
                                                 'domain_code' => 'ab',
                                                 'page_title' => 'title1',
-                                                'count_views' => '1234',
-                                                'total_response_size' => '100000'
+                                                'count_views' => '1234'
                                             ],
                                             [
                                                 'domain_code' => 'cd',
                                                 'page_title' => 'title2',
-                                                'count_views' => '5678',
-                                                'total_response_size' => '200000'
+                                                'count_views' => '5678'
                                             ]
                                         ]
                                     );
@@ -96,8 +94,8 @@ final class WikilogTest extends TestCase
                             . '表示する記事数を入力してください。' . PHP_EOL
                             . '2件の検索結果' . PHP_EOL
                             . '-----------------------------------------------------' . PHP_EOL
-                            . 'ab title1 1234 100000' . PHP_EOL
-                            . 'cd title2 5678 200000' . PHP_EOL
+                            . 'ab title1 1234' . PHP_EOL
+                            . 'cd title2 5678' . PHP_EOL
                             . '-----------------------------------------------------' . PHP_EOL
                             . '操作を選択してください。' . PHP_EOL
                             . '1 : ログ検索 その１' . PHP_EOL
@@ -106,10 +104,77 @@ final class WikilogTest extends TestCase
                             . 'アプリを終了します。' . PHP_EOL);
     }
 
-    // public function testStart_menu2(): void
-    // {
+    public function testStart_menu2(): void
+    {
+        WikilogTest::$simulator = new InputSimulator();
+        WikilogTest::$simulator->setValues([2, 'ab cd', 9]);
 
-    // }
+        $mockedDb = $this->getMockBuilder(\PDO::class)
+                        ->disableOriginalConstructor()
+                        ->disableArgumentCloning()
+                        ->disallowMockingUnknownTypes()
+                        ->getMock();
+
+        $mockedResult = new class extends \PDOStatement implements \IteratorAggregate
+                            {
+                                function rowCount(): int
+                                {
+                                    return 2;
+                                }
+
+                                function getIterator(): \Iterator
+                                {
+                                    return new \ArrayIterator(
+                                        [
+                                            [
+                                                'domain_code' => 'ab',
+                                                'total_views' => '1234'
+                                            ],
+                                            [
+                                                'domain_code' => 'cd',
+                                                'total_views' => '5678'
+                                            ]
+                                        ]
+                                    );
+                                }
+                            };
+
+        $sql = <<<EOT
+            SELECT domain_code, SUM(count_views) AS total_views
+            FROM logs
+            WHERE domain_code IN ('ab', 'cd')
+            GROUP BY domain_code
+            ORDER BY total_views DESC
+        EOT;
+
+        $mockedDb->method('query')
+        ->will($this->returnCallback(
+            function($arg) use($sql, $mockedResult)
+            {
+                return $arg === $sql? $mockedResult : new \PDOStatement();
+            }
+        ));
+
+        // ------------------------ Test ------------------------
+        $wikilog = new Wikilog($mockedDb);
+        $wikilog->start();
+
+        $this->expectOutputString('操作を選択してください。' . PHP_EOL
+                            . '1 : ログ検索 その１' . PHP_EOL
+                            . '2 : ログ検索 その２' . PHP_EOL
+                            . '9 : アプリを終了する' . PHP_EOL
+                            . '表示するドメインコードをスペース区切りで入力してください。' . PHP_EOL
+                            . '2件の検索結果' . PHP_EOL
+                            . '-----------------------------------------------------' . PHP_EOL
+                            . 'ab 1234' . PHP_EOL
+                            . 'cd 5678' . PHP_EOL
+                            . '-----------------------------------------------------' . PHP_EOL
+                            . '操作を選択してください。' . PHP_EOL
+                            . '1 : ログ検索 その１' . PHP_EOL
+                            . '2 : ログ検索 その２' . PHP_EOL
+                            . '9 : アプリを終了する' . PHP_EOL
+                            . 'アプリを終了します。' . PHP_EOL);
+    }
 
     // public function testStart_menu9(): void
     // {
