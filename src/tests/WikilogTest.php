3548a2b70b55dd6168cbcf4f9190ed5671262c92
root <root@DESKTOP-5KVMLKM>
2023-03-19 13:12:45 +0900

    testStart_menu1()が完成

--- src/tests/WikilogTest.php
+++ src/tests/WikilogTest.php
@@ -7,51 +7,85 @@ require_once __DIR__ . '/../app/Wikilog.php';
 use PHPUnit\Framework\TestCase;
 use Wikilog\Wikilog;
 
+class InputSimulator
+{
+    private array $values;
+
+    public function setValues(array $values): void
+    {
+        $this->values = $values;
+    }
+
+    public function next(): string
+    {
+        return (string)array_shift($this->values);
+    }
+}
+
 function fgets(): string
 {
-    return '1';
+    return WikilogTest::$simulator->next();
 }
 
 final class WikilogTest extends TestCase
 {
-    public function testStart(): void
+    public static InputSimulator $simulator;
+
+    public function testStart_menu1(): void
     {
+        WikilogTest::$simulator = new InputSimulator();
+        WikilogTest::$simulator->setValues([1, 2, 9]);
+
         $mockedDb = $this->getMockBuilder(\PDO::class)
                         ->disableOriginalConstructor()
                         ->disableArgumentCloning()
                         ->disallowMockingUnknownTypes()
                         ->getMock();
 
+        $mockedResult = new class extends \PDOStatement implements \IteratorAggregate
+                            {
+                                function rowCount(): int
+                                {
+                                    return 2;
+                                }
+
+                                function getIterator(): \Iterator
+                                {
+                                    return new \ArrayIterator(
+                                        [
+                                            [
+                                                'domain_code' => 'ab',
+                                                'page_title' => 'title1',
+                                                'count_views' => '1234',
+                                                'total_response_size' => '100000'
+                                            ],
+                                            [
+                                                'domain_code' => 'cd',
+                                                'page_title' => 'title2',
+                                                'count_views' => '5678',
+                                                'total_response_size' => '200000'
+                                            ]
+                                        ]
+                                    );
+                                }
+                            };
+
+        $sql = <<<EOT
+            SELECT domain_code, page_title, count_views
+            FROM logs
+            ORDER BY count_views DESC
+            LIMIT 2
+        EOT;
+
         $mockedDb->method('query')
-                ->willReturn(new class extends \PDOStatement implements \IteratorAggregate
-                    {
-                        function rowCount(): int
-                        {
-                            return 2;
-                        }
+        ->will($this->returnCallback(
+            function($arg) use($sql, $mockedResult)
+            {
+                return $arg === $sql? $mockedResult : new \PDOStatement();
+            }
+        ));
 
-                        function getIterator(): \Iterator
-                        {
-                            return new \ArrayIterator(
-                                [
-                                    [
-                                        'domain_code' => 'ab',
-                                        'page_title' => 'title1',
-                                        'count_views' => '1234',
-                                        'total_response_size' => '100000'
-                                    ],
-                                    [
-                                        'domain_code' => 'cd',
-                                        'page_title' => 'title2',
-                                        'count_views' => '5678',
-                                        'total_response_size' => '200000'
-                                    ]
-                                ]
-                            );
-                        }
-                    }
-                );
-
+        // ------------------------ Test ------------------------
         $wikilog = new Wikilog($mockedDb);
         $wikilog->start();
 
@@ -64,6 +98,21 @@ final class WikilogTest extends TestCase
                             . '-----------------------------------------------------' . PHP_EOL
                             . 'ab title1 1234 100000' . PHP_EOL
                             . 'cd title2 5678 200000' . PHP_EOL
-                            . '-----------------------------------------------------' . PHP_EOL);
+                            . '-----------------------------------------------------' . PHP_EOL
+                            . '操作を選択してください。' . PHP_EOL
+                            . '1 : ログ検索 その１' . PHP_EOL
+                            . '2 : ログ検索 その２' . PHP_EOL
+                            . '9 : アプリを終了する' . PHP_EOL
+                            . 'アプリを終了します。' . PHP_EOL);
     }
+
+    // public function testStart_menu2(): void
+    // {
+
+    // }
+
+    // public function testStart_menu9(): void
+    // {
+
+    // }
 }
